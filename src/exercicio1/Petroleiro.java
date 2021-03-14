1f4e72715d0836c9c9a22960ac2e19fe2e16537a
Tiago Santos <darish26@hotmail.com>
2021-03-14 13:56:41 +0000

    Commit inicial. Exercicio1 feito, com a classes criadas + os seus setters e construtores

---
+++ src/exercicio1/Petroleiro.java
@@ -0,0 +1,23 @@
+
+package exercicio1;
+
+
+public class Petroleiro extends Navio{
+    private float capacidade;
+    
+    public Petroleiro(String matricula, String nome, float comprimento, float capacidade){
+        super(matricula, nome, comprimento);
+        this.capacidade = capacidade;
+    }
+
+    public float getCapacidade() {
+        return capacidade;
+    }
+
+    public void setCapacidade(float capacidade) {
+        this.capacidade = capacidade;
+    }
+    
+ 
+    
+}
